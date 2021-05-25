6228d466c734d951de882fe91d56e21808db89a8
Oliver Odendaal <oliverode@gmail.com>
2021-05-25 19:38:41 -0400

    WIP - Start of threading code w/ new format

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ import shutil
 import time
 import glob
 from colorama import Fore, Back, Style
+import queue
+import threading
 
 eyed3.log.setLevel("ERROR") # Gives a bunch of invalid date warnings otherwise
-
 
 """
         FUNCTIONS
 """
+# Function that prints out a nice loading/procesing bar
 def print_percent_complete(index, total, length, name, compress, width):
     percent_complete = round(((index+1)/total * 100), 1)
     
@@ -38,6 +40,7 @@ def print_percent_complete(index, total, length, name, compress, width):
         else:
             print("")
 
+# Removes invalid characters from input string
 def removeBadChars(s):
     bad = ["/", "\\", ":", "*", "?", "<", ">", "|"]
     _s = list(s)
@@ -45,6 +48,37 @@ def removeBadChars(s):
         if _s[char] in bad:
             _s[char] = "-"
     return "".join(_s)
+
+# Assigns tasks to thread
+q = queue.Queue()
+def assign():
+    while True:
+        elem = q.get()
+        if elem is None:
+            break
+        copy_worker(elem)
+        q.task_done()
+
+# Copy task for putting file in correct file structure
+curCopy = 0
+def copy_worker(elem):
+    pass
+    # TODO: transfer the code over to index based
+    #   - Create missing directories
+    #   - Copy the file over
+    #   - Output
+    #   - Clean up the actual code
+
+    # for artist in outputStructure:
+    #     pathToArtist = os.path.join("Output", removeBadChars(artist[0]))
+    #     os.mkdir(pathToArtist)
+    #     for album in artist[1]:
+    #         pathToAlbum = os.path.join(pathToArtist, removeBadChars(album[0]))
+    #         os.mkdir(pathToAlbum)
+    #         for song in album[1]:
+    #             shutil.copy(song[1], pathToAlbum)
+    #             print_percent_complete(curCopy, totalSongs, barSize, "Copying", barSize <= 5, termSize[0])
+    #             curCopy += 1
 
 """
         MAIN PROCESSING
@@ -117,17 +151,23 @@ os.mkdir("Output")
 maxBar = termSize[0] - len("Finished copying: [] 100.0! complete") - 1
 barSize = min(maxBar, 75)
 
-curCopy = 0
-for artist in outputStructure:
-    pathToArtist = os.path.join("Output", removeBadChars(artist[0]))
-    os.mkdir(pathToArtist)
-    for album in artist[1]:
-        pathToAlbum = os.path.join(pathToArtist, removeBadChars(album[0]))
-        os.mkdir(pathToAlbum)
-        for song in album[1]:
-            shutil.copy(song[1], pathToAlbum)
-            print_percent_complete(curCopy, totalSongs, barSize, "Copying", barSize <= 5, termSize[0])
-            curCopy += 1
+threads = []
+THREAD_NUM = 3
 
+for i in range(THREAD_NUM):
+    t = threading.Thread(target=assign)
+    threads.append(t)
+    t.start()
+
+for artist_id in range(len(outputStructure)):
+    for album_id in range(len(outputStructure[artist_id])):
+        q.put((artist_id, album_id))
+q.join()
+
+for i in range(THREAD_NUM):
+    q.put(None)
+
+for t in threads:
+    t.join()
 
 print(f"\n{Fore.GREEN}Finished{Style.RESET_ALL}")
